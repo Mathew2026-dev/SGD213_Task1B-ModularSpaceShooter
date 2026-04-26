672ed10f2beb231cdc647fb24bcc5224eee31205
Mathew2026-dev <MDO004@student.usc.edu.au>
2026-04-26 10:06:34 +1000

    Imported "Inheritance" Unity Package
    -Added Pickup into the game
    -Removed duplicate code in Pickup script
    -fixed bug where bullets could colloid with pickup
    -Created new "Bullet" tag
    -a copy of PlayerMovement Script was created from the package and took the  place of the original, swapped it with the proper Playermovement script to fix the issue
    -removed unknown script that was added along with package import

--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -1,35 +1,88 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
-using System.Runtime.CompilerServices;
 using UnityEngine;
 
+/// <summary>
+/// PlayerInput handles all of the player specific input behaviour, and passes the input information
+/// to the appropriate scripts.
+/// </summary>
 public class PlayerInput : MonoBehaviour
 {
-    [SerializeField] private ShootingScript Shootscript;
 
-    // Start is called before the first frame update
+    // local references
+    private PlayerMovement playerMovement;
+    
+    private WeaponBase weapon;
+    public WeaponBase Weapon
+    {
+        get
+        {
+            return weapon;
+        }
+
+        set
+        {
+            weapon = value;
+        }
+    }
+
     void Start()
     {
-     
+        playerMovement = GetComponent<PlayerMovement>();
+        weapon = GetComponent<WeaponBase>();
     }
-    // Update is called once per frame
+
     void Update()
     {
-        if (Input.GetButton("Fire1"))
+        // read our horizontal input axis
+        float horizontalInput = Input.GetAxis("Horizontal");
+        // if movement input is not zero
+        if (horizontalInput != 0.0f)
         {
-            float CurrentTime = Time.time;
-            // Have a delay so we don't shoot too many bullets
-            // grabing the variables from the "ShootingScript" and using the information from it to set the fuctions
-            if (CurrentTime - Shootscript.lastFiredTime > Shootscript.fireDelay)
+            // ensure our playerMovementScript is populated to avoid errors
+            if (playerMovement != null)
             {
-                Vector2 spawnPosition = new Vector2(transform.position.x, transform.position.y + Shootscript.bulletOffset);
-
-                Instantiate(Shootscript.bullet, spawnPosition, transform.rotation);
-
-                Shootscript.lastFiredTime = CurrentTime;
-                //print("Shoot!");
-                print("Shoot");
+                // pass our movement input to our playerMovementScript
+                playerMovement.MovePlayer(horizontalInput * Vector2.right);
             }
         }
+
+        // if we press the Fire1 button
+        if (Input.GetButton("Fire1"))
+        {
+            // if our shootingScript is populated
+            if (weapon != null)
+            {
+                // tell shootingScript to shoot
+                weapon.Shoot();
+            }
+        }
+    }
+
+    /// <summary>
+    /// SwapWeapon handles creating a new WeaponBase component based on the given weaponType. This
+    /// will popluate the newWeapon's controls and remove the existing weapon ready for usage.
+    /// </summary>
+    /// <param name="weaponType">The given weaponType to swap our current weapon to, this is an enum in WeaponBase.cs</param>
+    public void SwapWeapon(WeaponType weaponType)
+    {
+        // make a new weapon dependent on the weaponType
+        WeaponBase newWeapon = null;
+        switch (weaponType)
+        {
+            case WeaponType.machineGun:
+                newWeapon = gameObject.AddComponent<WeaponMachineGun>();
+                break;
+            case WeaponType.tripleShot:
+                newWeapon = gameObject.AddComponent<WeaponTripleShot>();
+                break;
+        }
+
+        // update the data of our newWeapon with that of our current weapon
+        newWeapon.UpdateWeaponControls(weapon);
+        // remove the old weapon
+        Destroy(weapon);
+        // set our current weapon to be the newWeapon
+        weapon = newWeapon;
     }
 }
